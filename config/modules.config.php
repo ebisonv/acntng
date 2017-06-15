cb8dfb4ba64b41332b2eadfd40de1f4503a6e6d3
abisoninnov <abison@innovaturelabs.com>
2017-06-15 20:52:21 +0530

    added doctrine files

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,12 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Paginator',
     'Zend\Router',
     'Zend\Validator',
+    'DoctrineModule',
+    'DoctrineORMModule',
     'Application',
 ];
---
+++ data/cache/module-config-cache.application.config.cache.php
@@ -0,0 +1,425 @@
+<?php
+return array (
+  'service_manager' => 
+  array (
+    'aliases' => 
+    array (
+      'InputFilterManager' => 'Zend\\InputFilter\\InputFilterPluginManager',
+      'HttpRouter' => 'Zend\\Router\\Http\\TreeRouteStack',
+      'router' => 'Zend\\Router\\RouteStackInterface',
+      'Router' => 'Zend\\Router\\RouteStackInterface',
+      'RoutePluginManager' => 'Zend\\Router\\RoutePluginManager',
+    ),
+    'abstract_factories' => 
+    array (
+      0 => 'Zend\\InputFilter\\InputFilterAbstractServiceFactory',
+      'DoctrineModule' => 'DoctrineModule\\ServiceFactory\\AbstractDoctrineServiceFactory',
+    ),
+    'factories' => 
+    array (
+      'Zend\\InputFilter\\InputFilterPluginManager' => 'Zend\\InputFilter\\InputFilterPluginManagerFactory',
+      'FilterManager' => 'Zend\\Filter\\FilterPluginManagerFactory',
+      'Zend\\Paginator\\AdapterPluginManager' => 'Zend\\Paginator\\AdapterPluginManagerFactory',
+      'Zend\\Paginator\\ScrollingStylePluginManager' => 'Zend\\Paginator\\ScrollingStylePluginManagerFactory',
+      'Zend\\Router\\Http\\TreeRouteStack' => 'Zend\\Router\\Http\\HttpRouterFactory',
+      'Zend\\Router\\RoutePluginManager' => 'Zend\\Router\\RoutePluginManagerFactory',
+      'Zend\\Router\\RouteStackInterface' => 'Zend\\Router\\RouterFactory',
+      'ValidatorManager' => 'Zend\\Validator\\ValidatorPluginManagerFactory',
+      'doctrine.cli' => 'DoctrineModule\\Service\\CliFactory',
+      'Doctrine\\ORM\\EntityManager' => 'DoctrineORMModule\\Service\\EntityManagerAliasCompatFactory',
+    ),
+    'invokables' => 
+    array (
+      'DoctrineModule\\Authentication\\Storage\\Session' => 'Zend\\Authentication\\Storage\\Session',
+      'doctrine.dbal_cmd.runsql' => 'Doctrine\\DBAL\\Tools\\Console\\Command\\RunSqlCommand',
+      'doctrine.dbal_cmd.import' => 'Doctrine\\DBAL\\Tools\\Console\\Command\\ImportCommand',
+      'doctrine.orm_cmd.clear_cache_metadata' => 'Doctrine\\ORM\\Tools\\Console\\Command\\ClearCache\\MetadataCommand',
+      'doctrine.orm_cmd.clear_cache_result' => 'Doctrine\\ORM\\Tools\\Console\\Command\\ClearCache\\ResultCommand',
+      'doctrine.orm_cmd.clear_cache_query' => 'Doctrine\\ORM\\Tools\\Console\\Command\\ClearCache\\QueryCommand',
+      'doctrine.orm_cmd.schema_tool_create' => 'Doctrine\\ORM\\Tools\\Console\\Command\\SchemaTool\\CreateCommand',
+      'doctrine.orm_cmd.schema_tool_update' => 'Doctrine\\ORM\\Tools\\Console\\Command\\SchemaTool\\UpdateCommand',
+      'doctrine.orm_cmd.schema_tool_drop' => 'Doctrine\\ORM\\Tools\\Console\\Command\\SchemaTool\\DropCommand',
+      'doctrine.orm_cmd.convert_d1_schema' => 'Doctrine\\ORM\\Tools\\Console\\Command\\ConvertDoctrine1SchemaCommand',
+      'doctrine.orm_cmd.generate_entities' => 'Doctrine\\ORM\\Tools\\Console\\Command\\GenerateEntitiesCommand',
+      'doctrine.orm_cmd.generate_proxies' => 'Doctrine\\ORM\\Tools\\Console\\Command\\GenerateProxiesCommand',
+      'doctrine.orm_cmd.convert_mapping' => 'Doctrine\\ORM\\Tools\\Console\\Command\\ConvertMappingCommand',
+      'doctrine.orm_cmd.run_dql' => 'Doctrine\\ORM\\Tools\\Console\\Command\\RunDqlCommand',
+      'doctrine.orm_cmd.validate_schema' => 'Doctrine\\ORM\\Tools\\Console\\Command\\ValidateSchemaCommand',
+      'doctrine.orm_cmd.info' => 'Doctrine\\ORM\\Tools\\Console\\Command\\InfoCommand',
+      'doctrine.orm_cmd.ensure_production_settings' => 'Doctrine\\ORM\\Tools\\Console\\Command\\EnsureProductionSettingsCommand',
+      'doctrine.orm_cmd.generate_repositories' => 'Doctrine\\ORM\\Tools\\Console\\Command\\GenerateRepositoriesCommand',
+    ),
+  ),
+  'route_manager' => 
+  array (
+    'factories' => 
+    array (
+      'symfony_cli' => 'DoctrineModule\\Service\\SymfonyCliRouteFactory',
+    ),
+  ),
+  'router' => 
+  array (
+    'routes' => 
+    array (
+      'doctrine_orm_module_yuml' => 
+      array (
+        'type' => 'literal',
+        'options' => 
+        array (
+          'route' => '/ocra_service_manager_yuml',
+          'defaults' => 
+          array (
+            'controller' => 'DoctrineORMModule\\Yuml\\YumlController',
+            'action' => 'index',
+          ),
+        ),
+      ),
+      'home' => 
+      array (
+        'type' => 'Zend\\Router\\Http\\Literal',
+        'options' => 
+        array (
+          'route' => '/',
+          'defaults' => 
+          array (
+            'controller' => 'Application\\Controller\\IndexController',
+            'action' => 'index',
+          ),
+        ),
+      ),
+      'application' => 
+      array (
+        'type' => 'Zend\\Router\\Http\\Segment',
+        'options' => 
+        array (
+          'route' => '/application[/:action]',
+          'defaults' => 
+          array (
+            'controller' => 'Application\\Controller\\IndexController',
+            'action' => 'index',
+          ),
+        ),
+      ),
+    ),
+  ),
+  'doctrine' => 
+  array (
+    'cache' => 
+    array (
+      'apc' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\ApcCache',
+        'namespace' => 'DoctrineModule',
+      ),
+      'apcu' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\ApcuCache',
+        'namespace' => 'DoctrineModule',
+      ),
+      'array' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\ArrayCache',
+        'namespace' => 'DoctrineModule',
+      ),
+      'filesystem' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\FilesystemCache',
+        'directory' => 'data/DoctrineModule/cache',
+        'namespace' => 'DoctrineModule',
+      ),
+      'memcache' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\MemcacheCache',
+        'instance' => 'my_memcache_alias',
+        'namespace' => 'DoctrineModule',
+      ),
+      'memcached' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\MemcachedCache',
+        'instance' => 'my_memcached_alias',
+        'namespace' => 'DoctrineModule',
+      ),
+      'predis' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\PredisCache',
+        'instance' => 'my_predis_alias',
+        'namespace' => 'DoctrineModule',
+      ),
+      'redis' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\RedisCache',
+        'instance' => 'my_redis_alias',
+        'namespace' => 'DoctrineModule',
+      ),
+      'wincache' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\WinCacheCache',
+        'namespace' => 'DoctrineModule',
+      ),
+      'xcache' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\XcacheCache',
+        'namespace' => 'DoctrineModule',
+      ),
+      'zenddata' => 
+      array (
+        'class' => 'Doctrine\\Common\\Cache\\ZendDataCache',
+        'namespace' => 'DoctrineModule',
+      ),
+    ),
+    'authentication' => 
+    array (
+      'odm_default' => 
+      array (
+      ),
+      'orm_default' => 
+      array (
+        'objectManager' => 'doctrine.entitymanager.orm_default',
+      ),
+    ),
+    'authenticationadapter' => 
+    array (
+      'odm_default' => true,
+      'orm_default' => true,
+    ),
+    'authenticationstorage' => 
+    array (
+      'odm_default' => true,
+      'orm_default' => true,
+    ),
+    'authenticationservice' => 
+    array (
+      'odm_default' => true,
+      'orm_default' => true,
+    ),
+    'connection' => 
+    array (
+      'orm_default' => 
+      array (
+        'configuration' => 'orm_default',
+        'eventmanager' => 'orm_default',
+        'params' => 
+        array (
+          'host' => 'localhost',
+          'port' => '3306',
+          'user' => 'username',
+          'password' => 'password',
+          'dbname' => 'database',
+        ),
+      ),
+    ),
+    'configuration' => 
+    array (
+      'orm_default' => 
+      array (
+        'metadata_cache' => 'array',
+        'query_cache' => 'array',
+        'result_cache' => 'array',
+        'hydration_cache' => 'array',
+        'driver' => 'orm_default',
+        'generate_proxies' => true,
+        'proxy_dir' => 'data/DoctrineORMModule/Proxy',
+        'proxy_namespace' => 'DoctrineORMModule\\Proxy',
+        'filters' => 
+        array (
+        ),
+        'datetime_functions' => 
+        array (
+        ),
+        'string_functions' => 
+        array (
+        ),
+        'numeric_functions' => 
+        array (
+        ),
+        'second_level_cache' => 
+        array (
+        ),
+      ),
+    ),
+    'driver' => 
+    array (
+      'orm_default' => 
+      array (
+        'class' => 'Doctrine\\Common\\Persistence\\Mapping\\Driver\\MappingDriverChain',
+        'drivers' => 
+        array (
+        ),
+      ),
+    ),
+    'entitymanager' => 
+    array (
+      'orm_default' => 
+      array (
+        'connection' => 'orm_default',
+        'configuration' => 'orm_default',
+      ),
+    ),
+    'eventmanager' => 
+    array (
+      'orm_default' => 
+      array (
+      ),
+    ),
+    'sql_logger_collector' => 
+    array (
+      'orm_default' => 
+      array (
+      ),
+    ),
+    'mapping_collector' => 
+    array (
+      'orm_default' => 
+      array (
+      ),
+    ),
+    'formannotationbuilder' => 
+    array (
+      'orm_default' => 
+      array (
+      ),
+    ),
+    'entity_resolver' => 
+    array (
+      'orm_default' => 
+      array (
+      ),
+    ),
+    'migrations_configuration' => 
+    array (
+      'orm_default' => 
+      array (
+        'directory' => 'data/DoctrineORMModule/Migrations',
+        'name' => 'Doctrine Database Migrations',
+        'namespace' => 'DoctrineORMModule\\Migrations',
+        'table' => 'migrations',
+        'column' => 'version',
+      ),
+    ),
+    'migrations_cmd' => 
+    array (
+      'generate' => 
+      array (
+      ),
+      'execute' => 
+      array (
+      ),
+      'migrate' => 
+      array (
+      ),
+      'status' => 
+      array (
+      ),
+      'version' => 
+      array (
+      ),
+      'diff' => 
+      array (
+      ),
+      'latest' => 
+      array (
+      ),
+    ),
+  ),
+  'doctrine_factories' => 
+  array (
+    'cache' => 'DoctrineModule\\Service\\CacheFactory',
+    'eventmanager' => 'DoctrineModule\\Service\\EventManagerFactory',
+    'driver' => 'DoctrineModule\\Service\\DriverFactory',
+    'authenticationadapter' => 'DoctrineModule\\Service\\Authentication\\AdapterFactory',
+    'authenticationstorage' => 'DoctrineModule\\Service\\Authentication\\StorageFactory',
+    'authenticationservice' => 'DoctrineModule\\Service\\Authentication\\AuthenticationServiceFactory',
+    'connection' => 'DoctrineORMModule\\Service\\DBALConnectionFactory',
+    'configuration' => 'DoctrineORMModule\\Service\\ConfigurationFactory',
+    'entitymanager' => 'DoctrineORMModule\\Service\\EntityManagerFactory',
+    'entity_resolver' => 'DoctrineORMModule\\Service\\EntityResolverFactory',
+    'sql_logger_collector' => 'DoctrineORMModule\\Service\\SQLLoggerCollectorFactory',
+    'mapping_collector' => 'DoctrineORMModule\\Service\\MappingCollectorFactory',
+    'formannotationbuilder' => 'DoctrineORMModule\\Service\\FormAnnotationBuilderFactory',
+    'migrations_configuration' => 'DoctrineORMModule\\Service\\MigrationsConfigurationFactory',
+    'migrations_cmd' => 'DoctrineORMModule\\Service\\MigrationsCommandFactory',
+  ),
+  'controllers' => 
+  array (
+    'factories' => 
+    array (
+      'DoctrineModule\\Controller\\Cli' => 'DoctrineModule\\Service\\CliControllerFactory',
+      'Application\\Controller\\IndexController' => 'Zend\\ServiceManager\\Factory\\InvokableFactory',
+    ),
+  ),
+  'console' => 
+  array (
+    'router' => 
+    array (
+      'routes' => 
+      array (
+        'doctrine_cli' => 
+        array (
+          'type' => 'symfony_cli',
+        ),
+      ),
+    ),
+  ),
+  'form_elements' => 
+  array (
+    'aliases' => 
+    array (
+      'objectselect' => 'DoctrineModule\\Form\\Element\\ObjectSelect',
+      'objectradio' => 'DoctrineModule\\Form\\Element\\ObjectRadio',
+      'objectmulticheckbox' => 'DoctrineModule\\Form\\Element\\ObjectMultiCheckbox',
+    ),
+    'factories' => 
+    array (
+      'DoctrineModule\\Form\\Element\\ObjectSelect' => 'DoctrineORMModule\\Service\\ObjectSelectFactory',
+      'DoctrineModule\\Form\\Element\\ObjectRadio' => 'DoctrineORMModule\\Service\\ObjectRadioFactory',
+      'DoctrineModule\\Form\\Element\\ObjectMultiCheckbox' => 'DoctrineORMModule\\Service\\ObjectMultiCheckboxFactory',
+    ),
+  ),
+  'hydrators' => 
+  array (
+    'factories' => 
+    array (
+      'DoctrineModule\\Stdlib\\Hydrator\\DoctrineObject' => 'DoctrineORMModule\\Service\\DoctrineObjectHydratorFactory',
+    ),
+  ),
+  'view_manager' => 
+  array (
+    'template_map' => 
+    array (
+      'zend-developer-tools/toolbar/doctrine-orm-queries' => '/home/abison/www/inventory/vendor/doctrine/doctrine-orm-module/config/../view/zend-developer-tools/toolbar/doctrine-orm-queries.phtml',
+      'zend-developer-tools/toolbar/doctrine-orm-mappings' => '/home/abison/www/inventory/vendor/doctrine/doctrine-orm-module/config/../view/zend-developer-tools/toolbar/doctrine-orm-mappings.phtml',
+      'layout/layout' => '/home/abison/www/inventory/module/Application/config/../view/layout/layout.phtml',
+      'application/index/index' => '/home/abison/www/inventory/module/Application/config/../view/application/index/index.phtml',
+      'error/404' => '/home/abison/www/inventory/module/Application/config/../view/error/404.phtml',
+      'error/index' => '/home/abison/www/inventory/module/Application/config/../view/error/index.phtml',
+    ),
+    'display_not_found_reason' => true,
+    'display_exceptions' => true,
+    'doctype' => 'HTML5',
+    'not_found_template' => 'error/404',
+    'exception_template' => 'error/index',
+    'template_path_stack' => 
+    array (
+      0 => '/home/abison/www/inventory/module/Application/config/../view',
+    ),
+  ),
+  'zenddevelopertools' => 
+  array (
+    'profiler' => 
+    array (
+      'collectors' => 
+      array (
+        'doctrine.sql_logger_collector.orm_default' => 'doctrine.sql_logger_collector.orm_default',
+        'doctrine.mapping_collector.orm_default' => 'doctrine.mapping_collector.orm_default',
+      ),
+    ),
+    'toolbar' => 
+    array (
+      'entries' => 
+      array (
+        'doctrine.sql_logger_collector.orm_default' => 'zend-developer-tools/toolbar/doctrine-orm-queries',
+        'doctrine.mapping_collector.orm_default' => 'zend-developer-tools/toolbar/doctrine-orm-mappings',
+      ),
+    ),
+  ),
+);
